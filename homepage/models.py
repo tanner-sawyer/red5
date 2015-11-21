32f1e6038ce55fea8afe3a536f03a218cd6ff5ea
Joshhaws <josh.haws@byu.edu>
2015-11-21 11:05:00 -0700

    Update models.py

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Define models here
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Asset(models.Model):
+	asset_code = models.TextField(max_length=10, null=True, blank=True)
+	description = models.TextField(max_length=255, null=True, blank=True)
+	date_acquired = models.DateField(null=True, blank=True)
+	pass
+
+class Assignment(model.Model):
+	asset = models.ForeignKey(Asset)
+	location = models.TextField(max_length=30, null=True, blank=True)
+	organization_type = models.TextField(max_length=30, null=True, blank=True)
+	date_assigned = models.DateField(null=True, blank=True)
+	pass
+
+class Manufacturer(model.Model):
+	asset = models.ForeignKey(Asset)
+	manufacturer = models.TextField(max_length=30, null=True, blank=True)
+	part_num = models.TextField(max_length=30, null=True, blank=True)
+	pass
+
+class Maintenance(model.Model):
+	asset = models.ForeignKey(Asset)
+	maintenance_note = models.TextField(max_length=255, null=True, blank=True)
+	pass
