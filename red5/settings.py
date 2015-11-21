df9f0fe0575c16559ed1f32a9184b523c5d9c712
root <root@ip-172-31-18-63.us-west-2.compute.internal>
2015-11-21 19:22:24 +0000

    Add Beanstalk files

--- red5/settings.py
+++ red5/settings.py
@@ -59,17 +59,28 @@ WSGI_APPLICATION = 'red5.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'mydatabasename',
             'USER': 'databaseuser',
             'PASSWORD': 'secretpassword',
             'HOST': 'localhost',
             'PORT': '5432',
+        }
     }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
